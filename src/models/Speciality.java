9c02e68fa337a6583c26d317b6945922ebf64efb
jmarinrz <juan.marinr1@udea.edu.co>
2025-10-11 18:55:24 -0500

    Creacion de las primeras clases con sus getters y setters
    Se crearon las clase de las entidades del mundo con sus getters y setter primordiales

---
+++ src/models/Speciality.java
@@ -0,0 +1,43 @@
+package models;
+
+public class Speciality {
+    private int specialityId;
+    private String name;
+    private String description;
+
+    // Constructor vacío
+    public Speciality() {
+    }
+
+    // Constructor con todos los parámetros
+    public Speciality(int specialityId, String name, String description) {
+        this.specialityId = specialityId;
+        this.name = name;
+        this.description = description;
+    }
+
+    // Getters y Setters
+    public int getSpecialityId() {
+        return specialityId;
+    }
+
+    public void setSpecialityId(int specialityId) {
+        this.specialityId = specialityId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
